b5fe776f737b4a9f46bd182099a40438b8840fd0
GilgameshKingOfHeroes <tyousufleo8@gmail.com>
2016-10-15 15:47:54 -1000

    Add files via upload

---
+++ HANGBLADE.py
@@ -0,0 +1,8 @@
+import time
+
+def HangbladeMain():
+	Player1 = input("Welcome to the 5th Heaven's Feel of Fuyuki: Hangman's edition. Please enter your name: ")
+	Player2 = input("Now enter you're servant's class: ")
+	print("Now processsing inputted data.")
+	time.sleep(3)
+	print("In order to weed out the weak servants/masters from the crop, we've decided to instate a hangman ritual. You will enter a word for you're servant to guess. The 
